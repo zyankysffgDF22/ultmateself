a15df7ac35c902f4eee292cb1fdd774f21cb7c95
Ultimate-Self <108270274+zyankysffgDF22@users.noreply.github.com>
2024-09-02 22:14:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,8 +32,36 @@ setInterval(countdown, 1000);
 // Popup Form Open/Close Functions
 function openForm() {
     document.getElementById('popupForm').style.display = 'flex';
-}
+};
 
 function closeForm() {
     document.getElementById('popupForm').style.display = 'none';
-}
+};
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyYJG-oknZ6TKsUEd8LZnFHIbmK2GRJ4F0JLzC-wzufxDLG45xB5Yw3d2niGJaKjcoJ/exec'
+
+const form = document.forms['contact-form']
+
+ // Convert images to base64
+ Promise.all([getBase64(image1), getBase64(image2), getBase64(image3)])
+ .then((images) => {
+     data.image1 = images[0];
+
+     form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => alert("Thank you! your form is submitted successfully." ))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+      });
+
+ })
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+};
